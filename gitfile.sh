c1eb0780dafb79d8c2fc5edbe08e0d311f08afa4
rashi <rashimujumdar.gamil.com>
2023-02-20 02:01:01 +0530

    Test 2

--- gitfile.sh
+++ gitfile.sh
@@ -9,4 +9,4 @@ ddi=download
 if [ -d $ddi ]; then
 	rm -rf download
 fi
-git clone https://rashimujumdar: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@github.com/rashimujumdar/download.git
+git clone https://rashimujumdar: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@github.com/rashimujumdar/upload.git
