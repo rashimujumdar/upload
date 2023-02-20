f0e614060b51220d0f88bc1e13bdca0d4a64ed08
rashi <rashimujumdar.gamil.com>
2023-02-20 13:50:01 +0530

    Test 2

--- .gitfile.sh
+++ .gitfile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 cd ~/Public
+
 
 git add *.*
 git commit -m "Test 2"
-git push https://rashimujumdar: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@github.com/rashimujumdar/upload.git
+git push upload --force
+#https://rashimujumdar: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@github.com/rashimujumdar/upload.git
 
-ddi=download
+ddi='upload'
+
 if [ -d $ddi ]; then
-	rm -rf upload
+	rm -rf $ddi
 fi
+
 git clone https://rashimujumdar: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@github.com/rashimujumdar/upload.git
